fd4df6c6c02285d0b193e3d196d5c88a15d57d45
ivanna 50 <jana.bagin@gmail.com>
2023-02-04 19:40:50 +0200

    lesson5_useReducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-
 import './App.css';
 
-import React from 'react';
+import React, {useReducer} from 'react';
 
 const App = () => {
-  return (
-      <div>
-           App
-      </div>
-  );
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'INC1':
+                return {...state, count1: state.count1 + 1}
+            case 'DEC1':
+                return {...state, count2: state.count2 - 1}
+            case 'RESET':
+                return {count1:action.payload ,count2: action.payload}
+
+            default:
+                return {...state}
+        }
+    };
+    const initValue = () => (
+        {count1: 0, count2: 0}
+    );
+
+    const [state, dispatch] = useReducer(reducer, null, initValue);
+
+    return (
+        <div>
+            <div>Count1:{state.count1}</div>
+            <button onClick={()=> dispatch({type:'INC1'})}>inc</button>
+            <button onClick={()=> dispatch({type:'DEC1'})}>dec</button>
+            <button onClick={()=> dispatch({type:'RESET',payload:25})}>reset</button>
+            <div>Count2:{state.count2}</div>
+
+
+        </div>
+    );
 };
 
 
-
-export  {App};
+export {App};
